9bd408ed017cf3c090b75d21ec68b3bb4edf0a14
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2023-01-26 10:07:00 +0000

    menu mobile

---
+++ app.js
@@ -0,0 +1,14 @@
+let mobileMenu = document.querySelector(".mobile-menu")
+let mobileMenuLinks = document.querySelectorAll(".mobile-menu ul a")
+
+function openMobileMenu () {
+    mobileMenu.classList.add("active")
+}
+
+function closeMobileMenu () {
+    mobileMenu.classList.remove("active")
+}
+
+mobileMenuLinks.forEach(function(link) {
+    link.addEventListener("click", closeMobileMenu)
+})
