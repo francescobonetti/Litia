f31c5d451b2cfb78ce4ff4beac33c8eaf76ab53c
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2023-01-26 08:59:51 +0000

    resize cacellazione file inutili

--- main.js
+++ main.js
@@ -362,7 +362,16 @@ function desktopAnimation() {
 }
 
 
+window.addEventListener( 'resize', onWindowResize, false );
 
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
 
 
 
