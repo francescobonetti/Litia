8a40b31f354e8a8b52f9de07dfd1b077c6f861ab
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2023-01-29 00:00:51 +0000

    Sezione prodotto responsive

--- app.js
+++ app.js
@@ -22,3 +22,32 @@ function goToSpecta() {
     let spectaY = spectaSection.getBoundingClientRect().y
     window.scrollTo(0, spectaY, { duration: 10000 })
 }
+
+// PRODUCT GALLERY
+
+let mobileX = window.matchMedia("(max-width: 768px)")
+let galleryImg = Array.from(document.querySelector(".gallery").children)
+let imageW = document.querySelector(".gallery .span-2").getBoundingClientRect().width
+let gallery = document.querySelector(".gallery")
+let galleryIndex = 0
+
+if (mobileX.matches) {
+    scrollGallery()
+}
+
+function scrollGallery() {
+    setInterval(function(){
+        galleryIndex ++
+        if (galleryIndex == galleryImg.length) {galleryIndex = 0}
+        gallery.scrollTo(imageW * galleryIndex, 0)
+    }, 5000)
+}
+
+function draw() {
+    for(let i = 0; i <galleryImg.length; i++) {
+        let imgX = galleryImg[i].getBoundingClientRect().x
+        if ( imgX > 0 && imgX < gallery.getBoundingClientRect().width ){
+            galleryIndex = i
+        }
+    }
+}
