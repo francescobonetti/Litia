931797d551261956d666f42ec0ef776bba39a907
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2023-02-03 22:30:00 +0000

    bugs in giro

--- app.js
+++ app.js
@@ -22,3 +22,14 @@ function goToSpecta() {
     window.scrollTo(0, spectaY, { duration: 10000 })
 }
 
+let counterElem = document.querySelectorAll(".counter :nth-child(2)")
+let minus = document.querySelector(".plusminus:nth-child(1)")
+let plus = document.querySelector(".plusminus:nth-child(3)")
+let counter = 1 
+
+function increaseCounter() {if (counter == 9) {plus.style.opacity = 0.5; return};
+    counter ++; counterElem.forEach(function(elem){elem.innerHTML = counter;});
+    minus.style.opacity = 1;}
+function decreaseCounter() {if (counter == 0) {minus.style.opacity = 0.5;; return}
+    counter --; counterElem.forEach(function(elem){elem.innerHTML = counter;}); counterElem.innerHTML = counter;
+    plus.style.opacity = 1;}
